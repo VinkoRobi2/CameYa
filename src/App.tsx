68d68902ce243ac1eeb2caa89429fd9dd50d53f8
Ricardo <ricardopugar@gmail.com>
2025-10-30 11:08:38 -0500

    Intento de verificación

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import EmailCheck from "./pages/auth/EmailCheck";
 import VerifyEmail from "./pages/auth/VerifyEmail";
 import WorkerRegister from "./Register&Login/register/WorkerRegister";
@@ -7,32 +7,30 @@ import Protected from "./routes/Protected";
 import OnboardingGate from "./routes/OnboardingGate";
 import { WorkerPost } from "./Register&Login/register/WorkerPost";
 
-export default function AppRouter() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Auth */}
-        <Route path="/register" element={<WorkerRegister />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/check-email" element={<EmailCheck />} />
-        <Route path="/verify" element={<VerifyEmail />} />
+    <Routes>
+      {/* Auth */}
+      <Route path="/register" element={<WorkerRegister />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/check-email" element={<EmailCheck />} />
+      <Route path="/verify" element={<VerifyEmail />} />
 
-        {/* Onboarding protegido */}
-        <Route
-          path="/onboarding"
-          element={
-            <Protected>
-              <OnboardingGate>
-                <WorkerPost />
-              </OnboardingGate>
-            </Protected>
-          }
-        />
+      {/* Onboarding protegido */}
+      <Route
+        path="/onboarding"
+        element={
+          <Protected>
+            <OnboardingGate>
+              <WorkerPost />
+            </OnboardingGate>
+          </Protected>
+        }
+      />
 
-        {/* Dashboard y landing (placeholder) */}
-        <Route path="/dashboard" element={<div className="p-6">Dashboard</div>} />
-        <Route path="*" element={<div className="p-6">Landing</div>} />
-      </Routes>
-    </BrowserRouter>
+      {/* Dashboard y landing (placeholder) */}
+      <Route path="/dashboard" element={<div className="p-6">Dashboard</div>} />
+      <Route path="*" element={<div className="p-6">Landing</div>} />
+    </Routes>
   );
 }
